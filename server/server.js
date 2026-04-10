cd6e078e8771868ddfd9c7d1cd1db5b686f2cfd3
Sumanth9415 <potharajusumanth@gmail.com>
2026-04-10 15:58:06 +0530

    build: configure single-service deployment for client and server

--- server/server.js
+++ server/server.js
@@ -23,10 +23,10 @@ app.use('/api/submissions', require('./routes/submissionRoutes'));
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
   });
 }
 
